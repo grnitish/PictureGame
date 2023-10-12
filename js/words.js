d8be1266fd14f788147ae1502f588d0768820f80
grnitish <grnitish99@gmail.com>
2023-10-12 08:05:39 -0400

    IntroJs Functionality

--- js/words.js
+++ js/words.js
@@ -1,203 +1,477 @@
-let wordList = [
+let wordList1 = [
     {
         word: "python",
         hint: "programming language"
-        
-        
-        
+    },
+    {
+        word: "c",
+        hint: "Stacks and heaps are part of its memory management"
         
     },
     {
-        word: "guitar",
-        hint: "a musical instrument"
+        word: "typescript",
+        hint: "JavaScript's main competitor in web scripting"
         
     },
-    // {
-    //     word: "aim",
-    //     hint: "a purpose or intention"
-    // },
-    // {
-    //     word: "venus",
-    //     hint: "planet of our solar system"
-    // },
-    // {
-    //     word: "gold",
-    //     hint: "a yellow precious metal"
-    // },
-    // {
-    //     word: "ebay",
-    //     hint: "online shopping site"
-    // },
-    // {
-    //     word: "golang",
-    //     hint: "programming language"
-    // },
-    // {
-    //     word: "coding",
-    //     hint: "related to programming"
-    // },
-    // {
-    //     word: "matrix",
-    //     hint: "science fiction movie"
-    // },
-    // {
-    //     word: "bugs",
-    //     hint: "related to programming"
-    // },
-    // {
-    //     word: "avatar",
-    //     hint: "epic science fiction film"
-    // },
-    // {
-    //     word: "gif",
-    //     hint: "a file format for image"
-    // },
-    // {
-    //     word: "mental",
-    //     hint: "related to the mind"
-    // },
-    // {
-    //     word: "map",
-    //     hint: "diagram represent of an area"
-    // },
-    // {
-    //     word: "island",
-    //     hint: "land surrounded by water"
-    // },
-    // {
-    //     word: "hockey",
-    //     hint: "a famous outdoor game"
-    // },
-    // {
-    //     word: "chess",
-    //     hint: "related to a indoor game"
-    // },
-    // {
-    //     word: "viber",
-    //     hint: "a social media app"
-    // },
-    // {
-    //     word: "github",
-    //     hint: "code hosting platform"
-    // },
-    // {
-    //     word: "png",
-    //     hint: "a image file format"
-    // },
-    // {
-    //     word: "silver",
-    //     hint: "precious greyish-white metal"
-    // },
-    // {
-    //     word: "mobile",
-    //     hint: "an electronic device"
-    // },
-    // {
-    //     word: "gpu",
-    //     hint: "computer component"
-    // },
-    // {
-    //     word: "java",
-    //     hint: "programming language"
-    // },
-    // {
-    //     word: "google",
-    //     hint: "famous search engine"
-    // },
-    // {
-    //     word: "venice",
-    //     hint: "famous city of waters"
-    // },
-    // {
-    //     word: "excel",
-    //     hint: "microsoft product for windows"
-    // },
-    // {
-    //     word: "mysql",
-    //     hint: "a relational database system"
-    // },
-    // {
-    //     word: "nepal",
-    //     hint: "developing country name"
-    // },
-    // {
-    //     word: "flute",
-    //     hint: "a musical instrument"
-    // },
-    // {
-    //     word: "crypto",
-    //     hint: "related to cryptocurrency"
-    // },
-    // {
-    //     word: "tesla",
-    //     hint: "unit of magnetic flux density"
-    // },
-    // {
-    //     word: "mars",
-    //     hint: "planet of our solar system"
-    // },
-    // {
-    //     word: "proxy",
-    //     hint: "related to server application"
-    // },
-    // {
-    //     word: "email",
-    //     hint: "related to exchanging message"
-    // },
-    // {
-    //     word: "html",
-    //     hint: "markup language for the web"
-    // },
-    // {
-    //     word: "air",
-    //     hint: "related to a gas"
-    // },
-    // {
-    //     word: "idea",
-    //     hint: "a thought or suggestion"
-    // },
-    // {
-    //     word: "server",
-    //     hint: "related to computer or system"
-    // },
-    // {
-    //     word: "svg",
-    //     hint: "a vector image format"
-    // },
-    // {
-    //     word: "jpeg",
-    //     hint: "a image file format"
-    // },
-    // {
-    //     word: "search",
-    //     hint: "act to find something"
-    // },
-    // {
-    //     word: "key",
-    //     hint: "small piece of metal"
-    // },
-    // {
-    //     word: "egypt",
-    //     hint: "a country name"
-    // },
-    // {
-    //     word: "joker",
-    //     hint: "psychological thriller film"
-    // },
-    // {
-    //     word: "dubai",
-    //     hint: "developed country name"
-    // },
-    // {
-    //     word: "photo",
-    //     hint: "representation of person or scene"
-    // },
-    // {
-    //     word: "nile",
-    //     hint: "largest river in the world"
-    // },
-    // {
-    //     word: "rain",
-    //     hint: "related to a water"
-    // },
+    {
+        word: "r",
+        hint: "Popular language for data analysis and statistics"
+        
+    },
+    {
+        word: "swift",
+        hint: "Programming language for creating ios applications"
+        
+    },
+    {
+        word: "java",
+        hint: "Developed by Sun Microsystems, now owned by Oracle"
+        
+    },
+    {
+        word: "modular",
+        hint: "A concept that refers to breaking a complex problems into smaller, more manageable parts"
+        
+    },
+    {
+        word: "ruby",
+        hint: "Scripting language with a focus on simplicity"
+        
+    },
+    {
+        word: "haskell",
+        hint: "Functional language known for its strong typing"
+        
+    },
+    {
+        word: "nodejs",
+        hint: "Used extensively for web development, especially on the server-side"
+        
+    },]
+    
+let wordList2=[ 
+    {
+        word: "windows",
+        hint: "Popular operating system by Microsoft"
+    },
+    {
+        word: "photoshop",
+        hint: "Adobe's photo editing software"
+        
+    },
+    {
+        word: "zoom",
+        hint: "Video conferencing software"
+        
+    },
+    {
+        word: "chrome",
+        hint: "Web browser developed by Google"
+        
+    },
+    {
+        word: "office",
+        hint: "Microsoft's suite of office productivity software"
+        
+    },
+    {
+        word: "libreoffice",
+        hint: "Open-source office suite with a penguin mascot"
+        
+    },
+    {
+        word: "whatsapp",
+        hint: "Messaging app with a green logo, now owned by Facebook"
+        
+    },
+    {
+        word: "icloud",
+        hint: "Cloud storage service by Apple"
+        
+    },
+    {
+        word: "git",
+        hint: "Version control system for code collaboration"
+        
+    },
+    {
+        word: "excel",
+        hint: "spreadsheet application that is part of the Microsoft Office suite"
+        
+    },]
+
+let wordList3=[
+    {
+        word: "iphone",
+        hint: "Apple's flagship smartphone"
+    },
+    {
+        word: "playstation",
+        hint: "Gaming console by Sony, known for its exclusives"
+        
+    },
+    {
+        word: "nvidia",
+        hint: "Leading manufacturer of graphics processing units"
+        
+    },
+    {
+        word: "alienware",
+        hint: "High-end gaming PC manufacturer"
+        
+    },
+    {
+        word: "core",
+        hint: "Popular line of processors by Intel"
+        
+    },
+    {
+        word: "seagate",
+        hint: "Leading manufacturer of computer hard drives"
+        
+    },
+    {
+        word: "samsung",
+        hint: "South Korean electronics giant, known for phones and TVs"
+        
+    },
+    {
+        word: "razer",
+        hint: "Gaming hardware company known for its mice and keyboards"
+        
+    },
+    {
+        word: "hp",
+        hint: "American multinational corporation known for printers"
+        
+    },
+    {
+        word: "lenovo",
+        hint: "Chinese multinational company, the world's largest PC vendor"
+        
+    },]
+
+    let wordList4=[{
+        word: "twitter",
+        hint: "A platform known for its 140-character limit"
+    },
+    {
+        word: "facebook",
+        hint: "Owned by Mark Zuckerberg, it's the largest social network"
+        
+    },
+    {
+        word: "tiktok",
+        hint: "Short-form video app that gained popularity in 2020"
+        
+    },
+    {
+        word: "linkedin",
+        hint: "Professional networking platform for job seekers"
+        
+    },
+    {
+        word: "pinterest",
+        hint: "Image-centric platform with pins and boards"
+        
+    },
+    {
+        word: "snapchat",
+        hint: "Popular messaging app known for its disappearing messages"
+        
+    },
+    {
+        word: "youtube",
+        hint: "Video-sharing platform where creators can earn money"
+        
+    },
+    {
+        word: "oculus",
+        hint: "Owned by Meta Platforms, it's a virtual reality social platform"
+        
+    },
+    {
+        word: "instagram",
+        hint: "Photo and video sharing app with Stories feature"
+        
+    },
+    {
+        word: "tumblr",
+        hint: "Microblogging platform focused on multimedia content"
+        
+    },]
+
+let wordList5=[
+    {
+        word: "tony",
+        hint: "Billionaire genius with a suit"
+    },
+    {
+        word: "thor",
+        hint: "Mighty god of thunder"
+        
+    },
+    {
+        word: "Red and blue wall-crawler",
+        hint: "spidey"
+        
+    },
+    {
+        word: "Wakandan superhero and king",
+        hint: "tchalla"
+        
+    },
+    {
+        word: "Green, gamma-powered giant",
+        hint: "hulk"
+        
+    },
+    {
+        word: "Sorcerer Supreme",
+        hint: "strange"
+        
+    },
+    {
+        word: "Mutant with optic blasts",
+        hint: "cyclo"
+        
+    },
+    {
+        word: "Norse trickster and mischief-maker",
+        hint: "loki"
+        
+    },
+    {
+        word: "Feline superheroine with claws",
+        hint: "logan"
+        
+    },
+    {
+        word: "Super-soldier from World War II",
+        hint: "cap"
+        
+    },]
+
+
+//Rahul's words 
+
+//ANIME
+
+let wordList6=[
+    {
+        word: "naruto",
+        hint: "7th Hokage"
+    },
+    {
+        word: "luffy",
+        hint: "Pirate captain with a straw hat"
+    },
+    {
+        word: "light",
+        hint: "Shinigami who loves apples"
+    },
+    {
+        word: "edward",
+        hint: "Alchemist searching for the Philosopher's Stone"
+    },
+    {
+        word: "eva",
+        hint: "Giant robot pilot"
+    },
+    {
+        word: "sakura",
+        hint: "Magical girl with pink hair"
+    },
+    {
+        word: "yusuke",
+        hint: "Spirit detective with a green jacket"
+    },
+    {
+        word: "goku",
+        hint: "Saiyan warrior from Planet Vegeta"
+    },
+    {
+        word: "zoro",
+        hint: "Pirate swordsman with green hair"
+    },
+    {
+        word: "spike",
+        hint: "Bounty hunter in a yellow jumpsuit"
+    },
+]
+
+//NARUTO
+let wordList7=[
+    {
+        word: "naruto ",
+        hint: "7th Hokage"
+    },
+    {
+        word: "sasuke",
+        hint: "Sharingan user and last Uchiha"
+    },
+    {
+        word: "hinata",
+        hint: "Queen of huga clan"
+    },
+    {
+        word: "jiraya",
+        hint: "Legendary Sannin with white hair"
+    },
+    {
+        word: "kankuro",
+        hint: "Puppet master from the Sand Village"
+    },
+    {
+        word: "deidara",
+        hint: "Akatsuki member with clay explosives"
+    },
+    {
+        word: "guy",
+        hint: "Master of the Eight Gates"
+    },
+    {
+        word: "katsuya",
+        hint: "Tsunade's slug summon"
+    },
+    {
+        word: "shukaku",
+        hint: "Tailed Beast sealed within Gaara"
+    },
+    {
+        word: "kakashi",
+        hint: "Anbu captain with a mask"
+    },
+]
+
+//ONEPEICE
+let wordList8=[
+    {
+        word: "luffy",
+        hint: "Pirate captain with a straw hat"
+    },
+    {
+        word: "zoro",
+        hint: "Pirate swordsman with green hair"
+    },
+    {
+        word: "nami",
+        hint: "Navigator and cartographer"
+    },
+    {
+        word: "sanji",
+        hint: "strawhat crew cook"
+    },
+    {
+        word: "usopp",
+        hint: "long nose sniper"
+    },
+    {
+        word: "robin",
+        hint: "multiple hands historian"
+    },
+    {
+        word: "chopper",
+        hint: "reindeer doctor"
+    },
+    {
+        word: "brook",
+        hint: "musician with skeleton body"
+    },
+    {
+        word: "franky",
+        hint: "cyborg"
+    },
+    {
+        word: "jinbe",
+        hint: "Fish-man warrior with tattoos"
+    },
+]
+
+//NBA
+let wordList9=[
+    {
+        word: "lebron",
+        hint: "The King of the Lakers"
+    },
+    {
+        word: "curry",
+        hint: "Splash Brother with a deadly three-point shot."
+    },
+    {
+        word: "giannis",
+        hint: "The Greek Freak from the Bucks"
+    },
+    {
+        word: "harden",
+        hint: "The Beard and former Rocket"
+    },
+    {
+        word: "embiid",
+        hint: "The Process in Philadelphia"
+    },
+    {
+        word: "porzingis",
+        hint: "The Unicorn from Latvia"
+    },
+    {
+        word: "chris",
+        hint: "CP3 and Point God"
+    },
+    {
+        word: "davis",
+        hint: "The Brow and Lakers' big man"
+    },
+    {
+        word: "westbrook",
+        hint: " Mr.Triple-Doublein Washington"
+    },
+    {
+        word: "jokic",
+        hint: "The Joker from Denver."
+    },
+]
+
+//FOOD
+let wordList10=[
+    {
+        word: "lasgna",
+        hint: "Italian dish with layers of pasta, cheese, and sauce"
+    },
+    {
+        word: "sushi",
+        hint: "Japanese dish of vinegared rice with seafood and vegetables"
+    },
+    {
+        word: "nachos",
+        hint: "Mexican tortilla dish with cheese and various toppings"
+    },
+    {
+        word: "pbj",
+        hint: "Classic American sandwich with peanut butter and jelly"
+    },
+    {
+        word: "brie",
+        hint: "Creamy French cheese often used in desserts"
+    },
+    {
+        word: "carbonora",
+        hint: "Italian pasta dish with bacon, eggs, and cheese"
+    },
+    {
+        word: "chowmein",
+        hint: "Popular Chinese dish made with stir-fried noodles"
+    },
+    {
+        word: "roti",
+        hint: "Indian flatbread made from chickpea flour"
+    },
+    {
+        word: "paella",
+        hint: "Spanish rice dish often flavored with saffron"
+    },
+    {
+        word: "biryani",
+        hint: "Indian dish of rice with meat or vegetables, flavored with saffron or turmeric"
+    },
 ]
